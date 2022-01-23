4c7d8890d8b3a03591662a0e231e9d75c4465783
Mohamed Azekri <mohamed.azekri@student.rocva.nl>
2022-01-23 23:56:35 +0100

    eindopdracht

---
+++ Eindopdracht/eindopdracht.py
@@ -0,0 +1,6 @@
+leeftijd = int(input("Welkom bij rijschool 'Amsterdam', wat is jouw leeftijd? "))
+
+if leeftijd >= 17:
+    print("Jij bent oud genoeg om te beginnen met lessen.")
+else:
+    print("Helaas moet je nog even wachten voordat je de geschikte leeftijd heb bereikt.")
